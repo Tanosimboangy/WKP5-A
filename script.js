40b348491b6cff29ea51817f193bdf80a1ad8cfc
Tanosimboangy <jacquit.aja@onja.org>
2020-07-31 11:27:13 +0300

    looping through the object and creating the html, inserting the html inside the dom

--- script.js
+++ script.js
@@ -17,16 +17,16 @@ const recipes = [
 		id: 1596168482053,
 	},
 	{
-		title: 'Eggs',
+		title: 'Ravitoto',
 		picture: 'https://bit.ly/2ZXyiKI',
-		author: 'Loïc',
-		difficulty: 'easy',
-		timing: '15',
-		ingredients: ['eggs', 'salt', 'water'],
+		author: 'Jacquit',
+		difficulty: 'medium',
+		timing: '45',
+		ingredients: ['oil', 'salt', 'water', 'ravitoto'],
 		steps: [
 			'Put a pan on the fire',
-			'Crack the eggs on it',
-			'Wait, put them out',
+			'Put the ravitoto in the pot',
+			'Wait until it is cooked',
 			'Add some salt on it',
 		],
 		id: 1596168522409,
@@ -48,10 +48,27 @@ const recipes = [
 	},
 ];
 
-const renderCard = () => {
+const newContainer = document.querySelector(".container");
+const renderCard = (event) => {
+	console.log(event);
 	// check the recipes collection
-	// generate the HTML
-	// put it in the DOM
+	recipes.forEach(recipe => {
+		// generate the HTML
+		const myHTML = `
+			<div class="new_container">
+				<h1>"Hello! this is my recipe card"</h1>
+				<h2>${recipe.title}</h2>
+				<img src="${recipe.picture}">
+				<ul class="ul_container">
+					<li><p>${recipe.difficulty}</p></li>
+					<li><p>${recipe.timing}</p></li>
+				</ul>
+				<button class="more_info">More info</button>
+			</div>
+		`;
+		// put it in the DOM
+		document.body.innerHTML += (myHTML);
+	}); 
 };
 
 const generateButton = document.querySelector('button.generate');
