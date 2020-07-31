2dcc042c06567382b3897bee2a9cbb0835883e4c
Tanosimboangy <jacquit.aja@onja.org>
2020-07-31 13:01:17 +0300

    grabbing the more info button

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const recipes = [
 			'Wait, put them out',
 			'Add some salt on it',
 		],
-		id: 1596168482053,
+		id: 2544,
 	},
 	{
 		title: 'Ravitoto',
@@ -29,7 +29,7 @@ const recipes = [
 			'Wait until it is cooked',
 			'Add some salt on it',
 		],
-		id: 1596168522409,
+		id: 1129,
 	},
 	{
 		title: 'My recipe',
@@ -44,7 +44,21 @@ const recipes = [
 			'Wait, put them out',
 			'Add some salt on it',
 		],
-		id: 1596168522409,
+		id: 3089,
+	},
+	{
+		title: 'My new recipe',
+		picture: 'https://bit.ly/2ZXyiKI',
+		author: 'francois',
+		difficulty: 'difficult',
+		timing: '20',
+		ingredients: ['leaves', 'salt', 'water'],
+		steps: [
+			'Put a pan on the fire',
+			'Wait, put them out',
+			'Add some salt on it',
+		],
+		id: 5003,
 	},
 ];
 
@@ -55,7 +69,7 @@ const renderCard = (event) => {
 	recipes.forEach(recipe => {
 		// generate the HTML
 		const myHTML = `
-			<div class="new_container">
+			<div class="new_container" data-id="${recipe.id}">
 				<h1>"Hello! this is my recipe card"</h1>
 				<h2>${recipe.title}</h2>
 				<img src="${recipe.picture}">
@@ -67,9 +81,27 @@ const renderCard = (event) => {
 			</div>
 		`;
 		// put it in the DOM
-		document.body.innerHTML += (myHTML);
+		newContainer.insertAdjacentHTML('beforebegin', myHTML);
+		// document.body.innerHTML += (myHTML);
 	}); 
 };
 
 const generateButton = document.querySelector('button.generate');
 generateButton.addEventListener('click', renderCard);
+
+const infoButton = document.querySelector('.more_info');
+
+const realRecipe = (event) => {
+	console.log(event)
+	if(e.target.matches("button.more_info")) {
+
+	}	
+}
+infoButton.addEventListener('click', realRecipe);
+
+
+// const parent = e.target.closest(".new_container");
+// const id = Number(parent.dataset.id);
+// const recipe = recipes.find(singleRecipe => singleRecipe.id === id);
+// openModal(recipe);
+
